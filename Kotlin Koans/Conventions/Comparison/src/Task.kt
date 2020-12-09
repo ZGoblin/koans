43b7bebf20ed678dc5220ca41d34f957a807a327
ZGoblin <59374316+ZGoblin@users.noreply.github.com>
2020-12-09 23:31:14 +0200

    15 is good

--- Kotlin Koans/Conventions/Comparison/src/Task.kt
+++ Kotlin Koans/Conventions/Comparison/src/Task.kt
@@ -1,5 +1,13 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override operator fun compareTo(other: MyDate): Int {
+        if (year - other.year == 0) {
+            if (month - other.month == 0) {
+                return dayOfMonth - other.dayOfMonth
+            }
+            return month - other.month
+        }
+        return year - other.year
+    }
 }
 
 fun test(date1: MyDate, date2: MyDate) {
